7cfb5d6c69740921470236a1f83ca7416ed13b31
Caleb James <calebsjames@gmail.com>
2021-01-24 13:26:17 -0600

    save file fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
-cj-glassdaleCSS
 import { CriminalList } from "./criminals/CriminalList.js";
-
 CriminalList()
-=======
-import { getOfficers } from './officers/officerDataProvider.js'
-getOfficers();
 
-import { getCriminals, useCriminals } from './criminals/CriminalDataProvider.js'
-getCriminals();
-useCriminals();
-
-
-import { CriminalList } from "./criminals/CriminalList.js";
-
-criminalList()
-console.log(useCriminals())
-main
+// import { OfficerList } from "./officers/OfficerList.js";
+// OfficerList()
