74b40da0c2aa9991010539bd358cd74a2e6bd52d
Caleb James <calebsjames@gmail.com>
2021-01-27 11:35:44 -0600

    Returns criminal list filtered by conviction

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,6 +1,8 @@
-import {getCriminals, useCriminals} from "./CriminalDataProvider.js"
-import {Criminals} from "./Criminal.js"
+import { getCriminals, useCriminals } from "./CriminalDataProvider.js"
+import { Criminals } from "./Criminal.js"
+import { useConvictions } from "../convictions/ConvictionProvider.js"
 
+const eventHub = document.querySelector(".container")
 const criminalContainer = document.querySelector(".criminalsContainer")
 
 
@@ -9,17 +11,41 @@ export const CriminalList = () => {
     getCriminals()
         .then(() => {
             const criminalArray = useCriminals()
-
-            let criminalHTMLRepresentation = ""
-
-            for (const criminal of criminalArray) {
-                criminalHTMLRepresentation += Criminals(criminal)
-            }
-
-            criminalContainer.innerHTML = `
-                <h3>Criminals</h3>
-                <section class="criminalList">
-                ${criminalHTMLRepresentation}
-                </section>`
+            renderToDom(criminalArray)
         })
 }
+
+const renderToDom = (criminalCollection) => {
+
+    let criminalHTMLRepresentation = ""
+    
+    for (const criminal of criminalCollection) {
+        criminalHTMLRepresentation += Criminals(criminal)
+    }
+    
+    criminalContainer.innerHTML = `
+        <h3>Criminals</h3>
+        <section class="criminalList">
+        ${criminalHTMLRepresentation}
+        </section>`
+}
+
+eventHub.addEventListener("crimeChosen", crimeChosenEvent => {
+    if (crimeChosenEvent.detail.crimeThatWasChosen !== "0") {
+        
+        const convictionsArray = useConvictions()
+        
+        const chosenConvictionObject = convictionsArray.find(convictionObj => {
+            console.log("currently checking", convictionObj)
+            return convictionObj.id === parseInt(crimeChosenEvent.detail.crimeThatWasChosen)
+        })
+        console.log(chosenConvictionObject.name)
+        
+        debugger
+        const criminalArray = useCriminals()
+
+        const filteredCriminalsArray = criminalArray.filter(criminalObj => criminalObj.conviction === chosenConvictionObject.name)
+
+        renderToDom(filteredCriminalsArray)
+    }
+})
