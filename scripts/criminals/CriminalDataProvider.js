98ab47d24559d115436b4a902e1fe3b3323588be
Caleb James <calebsjames@gmail.com>
2021-01-27 13:36:43 -0600

    added OfficerList and DataProvider content

--- scripts/criminals/CriminalDataProvider.js
+++ scripts/criminals/CriminalDataProvider.js
@@ -5,10 +5,5 @@ export const useCriminals = () => criminals.slice();
 export const getCriminals = () => {
     return fetch("https://criminals.glassdale.us/criminals")
         .then(response => response.json())
-        .then(
-            parsedCriminals => {
-                console.table(parsedCriminals)
-                criminals = parsedCriminals
-            }
-        )
+        .then(parsedCriminals => criminals = parsedCriminals)
 }
