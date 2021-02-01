bfcb7b8173d131699fe91e55d995fb426ce529fa
Caleb James <calebsjames@gmail.com>
2021-01-31 22:30:10 -0600

    fixing note files

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -40,3 +40,7 @@ eventHub.addEventListener("noteStateChanged", event => {
         NoteList()
     }
 })
+
+eventHub.addEventListener("clearNotesClicked", customEvent => {
+    noteContainerTarget.innerHTML = ""
+})
--- scripts/notes/ShowNotesButton.js
+++ scripts/notes/ShowNotesButton.js
@@ -1,6 +1,14 @@
 const contentTarget = document.querySelector(".noteListButton")
 const eventHub = document.querySelector(".container")
 
+export const ShowNoteButton = () => {
+    contentTarget.innerHTML = 
+    `
+    <button id='showNotes'>Show Notes</button>
+    <button id='clearNotes'>Clear Notes</button>
+    `
+}
+
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "showNotes") {
         const customEvent = new CustomEvent("showNotesClicked")
@@ -8,6 +16,9 @@ eventHub.addEventListener("click", clickEvent => {
     }
 })
 
-export const ShowNoteButton = () => {
-    contentTarget.innerHTML = "<button id='showNotes'>Show Notes</button>"
-}
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "clearNotes") {
+        const customEvent = new CustomEvent("clearNotesClicked")
+        eventHub.dispatchEvent(customEvent)
+    }
+})
