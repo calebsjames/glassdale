7264018e8c976fc99fe007eb18775f3979dd367a
Caleb James <calebsjames@gmail.com>
2021-01-31 22:21:58 -0600

    nothing to see here

---
+++ scripts/witnesses/WitnessDataProvider.js
@@ -0,0 +1,9 @@
+let witnesses = [];
+
+export const useWitness = () => witnesses.slice();
+
+export const getWitnesses = () => {
+    return fetch("https://criminals.glassdale.us/witnesses")
+        .then(response => response.json())
+        .then(parsedWitnesses => witnesses = parsedWitnesses)
+}
