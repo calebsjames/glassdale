11d66bea80eb342bc2e265839c685ff77c8d109c
Caleb James <calebsjames@gmail.com>
2021-02-12 14:31:54 -0600

    criminal list update

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -5,6 +5,7 @@ import { useConvictions } from "../convictions/ConvictionProvider.js"
 import { getWitnesses, useWitness } from "../witnesses/WitnessDataProvider.js"
 import { Witness } from "../witnesses/Witnesses.js"
 import { getCriminalFacilities, useCriminalFacilities } from "../facilities/CriminalFacilityDataProvider.js"
+import { useFacilities } from "../facilities/FacilityDataProvider.js"
 
 
 //define event hub for listening
@@ -13,45 +14,104 @@ const eventHub = document.querySelector(".container")
 //define location that will house "criminalList" which houses criminal cards
 const criminalContainer = document.querySelector(".criminalsContainer")
 
-//get and use criminalFacilities
+
+// //export function to put criminal slices on DOM
+// export const CriminalList = () => {
+//     //gets criminals from API
+//     getCriminals()
+//     //house sliced criminals in criminalArray and facilities 
+//     .then(() => {
+//         const criminalArray = useCriminals()
+
+//         //get and use criminalFacilities
+//         const criminalFacilities = getCriminalFacilities()
+//             .then(() => {
+//                 const facilityArray = useCriminalFacilities()
+//                 debugger
+//                 //give criminalArray to criminalToDOM 
+//                 criminalToDom(criminalArray, facilityArray)
+//             })
+            
+//         })
+// }
+
+
+
+
 
 //export function to put criminal slices on DOM
 export const CriminalList = () => {
-    //gets criminals from API
-    getCriminals()
-    //house sliced criminals in criminalArray and facilities 
-    .then(() => {
-        const criminalArray = useCriminals()
-        const criminalFacilities = getCriminalFacilities()
-            .then(() => {
-                const facilityArray = useCriminalFacilities()
-                debugger
-                //give criminalArray to criminalToDOM 
-                criminalToDom(criminalArray, facilityArray)
-            })
-            
-        })
+    // Kick off the fetching of both collections of data
+    getFacilities()
+        .then(getCriminalFacilities)
+        .then(() => {
+                // Pull in the data now that it has been fetched
+                const facilities = useFacilities()
+                const crimFac = useCriminalFacilities()
+                const criminals = useCriminals()
+
+                // Pass all three collections of data to render()
+                criminalToDom(criminals, facilities, crimFac)
+            }
+        )
 }
 
 
 
-//define function to put sliced data on DOM
-const criminalToDom = (criminalCollection, facility) => {
-    //defin an empty string to house the HTML representation
-    let criminalHTMLRepresentation = ""
-    //iterate through criminalArray and add each criminal to the HTMLRep
-    for (const criminal of criminalCollection) {
-        criminalHTMLRepresentation += Criminals(criminal)
-    }
+
+
+
+
+
+
+const criminalToDOM = (criminalsToRender, allFacilities, allRelationships) => {
+    // Step 1 - Iterate all criminals
+    contentTarget.innerHTML = criminalsToRender.map(
+        (criminalObject) => {
+            // Step 2 - Filter all relationships to get only ones for this criminal
+            const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+
+            // Step 3 - Convert the relationships to facilities with map()
+            const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+                const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+                return matchingFacilityObject
+            })
+
+            // Must pass the matching facilities to the Criminal component
+            return Criminal(criminalObject, facilities)
+        }
+    ).join("")
+}
+
+
+
+// //define function to put sliced data on DOM
+// const criminalToDom = (criminalCollection, facilityCollection, criminalInFacilityCollection) => {
+//     //defin an empty string to house the HTML representation
+//     let criminalHTMLRepresentation = ""
+//     //iterate through criminalArray and add each criminal to the HTMLRep
+//     for (const criminal of criminalCollection) {
+//         criminalHTMLRepresentation += Criminals(criminal)
+//     }
 
     
-    criminalContainer.innerHTML = `
-        <button id="criminalButton">Criminals</button>
-        <button id="witnessButton">Witnesses</button>
-        <section class="criminalList">
-        ${criminalHTMLRepresentation}
-        </section>`
-}
+//     criminalContainer.innerHTML = `
+//         <button id="criminalButton">Criminals</button>
+//         <button id="witnessButton">Witnesses</button>
+//         <section class="criminalList">
+//         ${criminalHTMLRepresentation}
+//         </section>`
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 //event listener to filter criminal list by crime
@@ -74,12 +134,19 @@ eventHub.addEventListener("crimeChosen", crimeChosenEvent => {
 })
 
 
+
+
+
+
+
 //event listener to filter criminals by arresting officer
 eventHub.addEventListener("officerSelect", officerChosenEvent => {
     if (officerChosenEvent.detail.selectedOfficer !== "0") {
         
         const criminals = useCriminals()
-        
+        const facilities = useFacilities()
+        const criminalInFacility = useCriminalFacilities()
+
         const officerName = officerChosenEvent.detail.selectedOfficer
 
         const chosenOfficerObject = criminals.filter(
